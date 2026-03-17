3634e46c94b70b7e395a83814e6297a5afeb6dd7
P4uLT <7680607+P4uLT@users.noreply.github.com>
2026-03-17 16:21:49 +0100

    refactor: remove duplicate LovelaceGridOptions

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,7 @@
 export type { HomeAssistant } from './ha/types';
 export type { HassEntity } from 'home-assistant-js-websocket';
 export type { LovelaceCard, LovelaceCardEditor } from './ha/panels/lovelace/types';
-export type { LovelaceCardConfig, ActionConfig } from './ha/data/lovelace';
+export type { LovelaceCardConfig, ActionConfig, LovelaceGridOptions } from './ha/data/lovelace';
 
 // ─── Project Types ─────────────────────────────────────────────────────────────
 
@@ -15,14 +15,3 @@ export type { CurveCardConfig } from './cards/curve-card/curve-card-config';
 export type { Layout } from './utils/layout';
 export type { Info, IconType } from './utils/info';
 export type { AppearanceSharedConfig, Appearance } from './shared/config/appearance-config';
-
-// ─── Lovelace Grid Options ────────────────────────────────────────────────────
-
-export interface LovelaceGridOptions {
-  columns?: number;
-  rows?: number;
-  min_columns?: number;
-  max_columns?: number;
-  min_rows?: number;
-  max_rows?: number;
-}
